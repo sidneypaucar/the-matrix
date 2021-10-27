249bedea0dfada0cb9abea14c6555df9b613786c
Sidney Paucar <sidneypaucar@sidneys-mbp-2.myfiosgateway.com>
2021-10-27 11:46:21 -0400

    added delete button

--- src/components/BlueFeed.js
+++ src/components/BlueFeed.js
@@ -1,10 +1,16 @@
 const BlueFeed = ({postData}) => {
+  
+  const deleteUserComment = () => {
+    console.log('deleting');
+  };
+
   return (
     <div>
       <h1>User: {postData.fields.user}</h1>
       <p>{postData.fields.comment}</p>
+      <button onClick={deleteUserComment}>Delete</button>
     </div>
-  )
+  ) 
 }
 
 export default BlueFeed;
