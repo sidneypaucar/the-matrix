c98d78eadab9c990102f181d1db87d099e47b306
Sidney Paucar <sidneypaucar@sidneys-mbp-2.myfiosgateway.com>
2021-10-29 03:20:51 -0400

    styling

--- src/components/menu.js
+++ src/components/menu.js
@@ -3,10 +3,9 @@ import "../App.css";
 const Menu = () => {
   return (
     <div>
-      <h3>This is your last chance. <br/>After this there is no turning back. <br/>You take the blue pill, the story ends;<br/> you wake up in your bed and <br/>believe whatever you want to believe. <br /> You take the red pill</h3>
+      <h3>This is your last chance. <br/>After this there is no turning back. <br/>You take the blue pill, the story ends;<br/> you wake up in your bed and <br/>believe whatever you want to believe. <br /> You take the red pill, you stay in Wonderland.</h3>
           <h1> Which Will You Decide? </h1>
     </div>
   ) 
 }
-
 export default Menu;
