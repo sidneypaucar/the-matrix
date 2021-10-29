4eac46b2a53f6c24cd189bfaefd881f944a576ed
Sidney Paucar <sidneypaucar@sidneys-mbp-2.myfiosgateway.com>
2021-10-29 02:27:52 -0400

    Added typewriter effect to user comment

--- src/components/menu.js
+++ src/components/menu.js
@@ -3,7 +3,7 @@ import "../App.css";
 const Menu = () => {
   return (
     <div>
-      <h3>This is your last chance. <br/>After this there is no turning back. <br/>You take the blue pill, the story ends;<br/> you wake up in your bed and <br/>believe whatever you want to believe.</h3>
+      <h3>This is your last chance. <br/>After this there is no turning back. <br/>You take the blue pill, the story ends;<br/> you wake up in your bed and <br/>believe whatever you want to believe. <br /> You take the red pill</h3>
           <h1> Which Will You Decide? </h1>
     </div>
   ) 
